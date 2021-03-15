35018c0b19f2f52d816ce47af6d69305533c4079
Mustufa Qadri <80328402+mustufa-sibisoft@users.noreply.github.com>
2021-03-15 12:06:21 +0500

    second

--- src/BasicConcepts.java
+++ src/BasicConcepts.java
@@ -3,7 +3,7 @@ public class BasicConcepts {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("fixed");
+		System.out.print("fixednow");
 	}
 
 }
